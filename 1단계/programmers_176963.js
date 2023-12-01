aab021cd3b934cdb477dedf639a3411836c8c5a9
KANGYONGSU23 <edc20203@gmail.com>
2023-12-01 20:28:07 +0900

    추억 점수

---
+++ 1단계/programmers_176963.js
@@ -0,0 +1,12 @@
+function solution(name, yearning, photo) {
+  const answer = [];
+  for (let i = 0; i < photo.length; i++) {
+    answer[i] = photo[i].reduce((acc, ele) => {
+      if (name.findIndex((name) => name === ele) !== -1) {
+        return acc + yearning[name.findIndex((name) => name === ele)];
+      }
+      return acc;
+    }, 0);
+  }
+  return answer;
+}
