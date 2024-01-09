492c982c6a6441660b7d8784ed83993891fa0496
KANGYONGSU23 <edc20203@gmail.com>
2024-01-10 08:57:33 +0900

    소수찾기

---
+++ 1단계/programmers_12977.js
@@ -0,0 +1,10 @@
+function solution(nums) {
+  let cnt = 0;
+  const even = nums.filter((item) => item % 2 === 0);
+  const odd = nums.filter((item) => item % 2 !== 0);
+
+  
+
+}
+
+console.log(solution([1, 2, 3, 4]));
