1a5ebe7fa6d1583f0b98299a8a44f88f0b8d0263
KangYongSu <edc20203@gmail.com>
2024-01-25 21:12:48 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,18 +1,32 @@
-//선택정렬
-function InsertionSort(array) {    
-    for(let i = 1 ; i< array.length ;i++){
-        for(let j = 0; j < i; j++) {
-            if(array[j] > array[i]){
-                let tmp = array[i];
-                for(let k = i; k > j ; k--){
-                    array[k] = array[k-1];
-                }
-                array[j] = tmp;
-            }
-            console.log(i, j, array)
+function Merge(leftArray, rightArray) {
+    let array = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+    
+    while(leftIndex < leftArray.length && rightIndex < rightArray.length){
+        if(leftArray[leftIndex] < rightArray[rightIndex]){
+            array.push(leftArray[leftIndex]);
+            leftIndex++;
+        }
+        else {
+            array.push(rightArray[rightIndex]);
+            rightIndex++;
         }
     }
-    return array
+    
+    return array.concat(leftArray.slice(leftIndex)).concat(rightArray.slice(rightIndex));
+}
+
+function MergeSort(array) {    
+    if(array.length <= 1) {
+        return array;
+    }
+    
+    let midleIndex = Math.floor(array.length / 2);  
+    let left = array.slice(0, midleIndex);
+    let right = array.slice(midleIndex);
+    
+    return Merge(MergeSort(left), MergeSort(right));
 }
 
 function solution(array, commands) {
@@ -20,70 +34,7 @@ function solution(array, commands) {
     for(let [i, j, k] of commands){
         const sortArray = array.slice(i-1, j);
         
-        result.push(InsertionSort(array.slice(i-1, j))[k-1])
+        result.push(MergeSort(array.slice(i-1, j))[k-1])
     }
     return result
-}
-
-
-//삽입정렬
-function InsertionSort(array) {
-    for(let i = 1 ; i< array.length ;i++){
-        for(let j = 0; j < i; j++) {
-            if(array[j] > array[i]){
-                let tmp = array[i];
-                for(let k = i; k > j ; k--){
-                    array[k] = array[k-1];
-                }
-                array[j] = tmp;
-            }
-            console.log(i, j, array)
-        }
-    }
-    return array
-}
-
-function solution(array, commands) {
-    let result = [];
-    for(let [i, j, k] of commands){
-        const sortArray = array.slice(i-1, j);
-
-        result.push(InsertionSort(array.slice(i-1, j))[k-1])
-    }
-    return result
-}
-
-
-//버블정렬
-function BubbleSort(array) {
-    for(let i in array){
-        for(let j = 0; j < array.length -1; j++){
-            if(array[j] > array[j+1]){
-                let tmp = array[j];
-                array[j] = array[j+1];
-                array[j+1] = tmp;
-            }
-        }
-    }
-    return array
-}
-
-function solution(array, commands) {
-    let result = [];
-    for(let [i, j, k] of commands){
-        const sortArray = array.slice(i-1, j);
-
-        result.push(BubbleSort(array.slice(i-1, j))[k-1])
-    }
-    return result
-}
-
-
-//메소드 사용
-function solution(array, commands) {
-    var answer = [];
-    for(let [st,en,cnt] of commands) {
-        answer.push(array.slice(st-1, en).sort((a,b)=>a-b)[cnt-1])
-    }
-    return answer
 }
