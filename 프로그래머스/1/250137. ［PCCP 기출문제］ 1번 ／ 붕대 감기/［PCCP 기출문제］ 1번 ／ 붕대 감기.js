2e0ec0edef476fcd60a6b169de873193bfc1e6de
KangYongSu <edc20203@gmail.com>
2024-04-18 10:05:12 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 2.59 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -5,16 +5,13 @@ function solution(bandage, health, attacks) {
     let end_of_game = true;
     attacks.some(([time, dmg], idx)=>{
         health += ((time - 1 - prev_attack_time) * recovery);
-        console.log(`+ ${(time - 1 - prev_attack_time) * recovery}`)
         
         if(time - 1 - prev_attack_time >= skill_time) {
             const cnt = ~~((time - 1 - prev_attack_time) / skill_time)
-            console.log(`보너스 * ${cnt}`)
             health += (bonus * cnt);
         }
         
         if(health > max_health) {
-            console.log('가득')
             health = max_health;
         }
         
@@ -22,7 +19,6 @@ function solution(bandage, health, attacks) {
         console.log(`공격 -${dmg}`)
         
         if(health <= 0) {
-            console.log('체력 없음 게임 끗')
             end_of_game = false;
             return true;
         }
