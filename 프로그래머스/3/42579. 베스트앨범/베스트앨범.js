0b660eeea802b7daf4b27ade301d8925a5cd9d49
KangYongSu <edc20203@gmail.com>
2024-05-09 09:13:34 +0900

    [level 3] Title: 베스트앨범, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,27 @@
+function solution(genres, plays) {
+  const result = [];
+  const obj = {};
+  const info = {};
+
+  for (let i = 0; i < genres.length; i++) {
+    obj[genres[i]] = (obj[genres[i]] || 0) + plays[i];
+    info[i] = { play: plays[i], genre: genres[i] };
+  } 
+
+  const sortedGenre = Object.keys(obj).sort((a, b) => obj[b] - obj[a]);
+
+  sortedGenre.forEach((genre) => {
+    const genreSong = Object.values(info)
+      .filter((song) => song.genre === genre)
+      .sort((a, b) => b.play - a.play)
+      .slice(0, 2);
+
+    genreSong.forEach((gs) => {
+      const uniqueId = Object.values(info).indexOf(gs);
+      result.push(uniqueId);
+    });
+  });
+
+  return result;
+}
+
